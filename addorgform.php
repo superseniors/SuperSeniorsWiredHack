0f5138d04cf40cd64825870a5e9ba42a4adcc5c8
stokesc6 <stokesc6@winthrop.edu>
2015-01-16 22:21:36 -0500

    finshed organization form

--- addorgform.php
+++ addorgform.php
@@ -17,15 +17,24 @@ require 'saltgen.php';
 <head></head>
 <body>
 <form method="post" action="insertorg.php">
-    <input name="username" type="text">
-    <input name="password" type="password">
-    <input name="email" type="email">
-    <input name="address" type="text">
-    <input name="city" type="text">
-    <input name="state" type="text">
-    <input name="zip" type="text">
-    <input name="phone" type="text">
-    <input name="website" type="text">
+    <label for="username">username</label>
+    <input name="username" type="text"><br/><br/>
+    <label for="password">password</label>
+    <input name="password" type="password"><br/><br/>
+    <label for="email">email</label>
+    <input name="email" type="email"><br/><br/>
+    <label for="address">street address</label>
+    <input name="address" type="text"><br/><br/>
+    <label for="city">city</label>
+    <input name="city" type="text"><br/><br/>
+    <label for="state">state</label>
+    <input name="state" type="text"><br/><br/>
+    <label for="zip">zip</label>
+    <input name="zip" type="text"><br/><br/>
+    <label for="phone">phone</label>
+    <input name="phone" type="text"><br/><br/>
+    <label for="website">website URL</label>
+    <input name="website" type="text"><br/><br/>
     <input type="submit" name="Submit">
 </form>
 
